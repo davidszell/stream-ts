7bb15c0b7e22d70695f5378c6720e63fb180e03f
David Szell <mail@davidszell.com>
2022-03-27 11:47:33 +0200

    Added base functionality

---
+++ src/__tests__/index_spec.ts
@@ -0,0 +1,229 @@
+import Stream from '../';
+
+describe('Stream', () => {
+
+  it('exists', () => {
+    expect(Stream).toBeDefined();
+  });
+
+  it('creates a stream', () => {
+    const stream = new Stream();
+    expect(stream).toBeDefined();
+  });
+
+  it('creates a stream with item', () => {
+    const stream = new Stream(1);
+    expect(stream.toArray()).toEqual([1]);
+  });
+
+  it('creates stream with item and predicate', () => {
+    const stream = new Stream(1, () => new Stream(2));
+    expect(stream.toArray()).toEqual([1, 2]);
+  });
+
+  describe('fromArray', () => {
+    it('creates a stream from array', () => {
+      const stream = Stream.fromArray([1, 2, 3]);
+      expect(stream.toArray()).toEqual([1, 2, 3]);
+    });
+
+    it('creates a stream from empty array', () => {
+      const stream = Stream.fromArray([]);
+      expect(stream.toArray()).toEqual([]);
+    });
+  });
+
+  describe('first', () => {
+    it('returns first item', () => {
+      const stream = Stream.fromArray([1, 2, 3]);
+      expect(stream.first()).toEqual(1);
+    });
+
+    it('throws error if stream is empty', () => {
+      const stream = new Stream();
+      expect(() => stream.first()).toThrow();
+    });
+  });
+
+  describe('isEmpty', () => {
+    it('returns true if stream is empty', () => {
+      const stream = new Stream();
+      expect(stream.isEmpty()).toBeTruthy();
+    });
+
+    it('returns false if stream is not empty', () => {
+      const stream = Stream.fromArray([1, 2, 3]);
+      expect(stream.isEmpty()).toBeFalsy();
+    });
+  });
+
+  describe('toArray', () => {
+    it('returns array', () => {
+      const stream = Stream.fromArray([1, 2, 3]);
+      expect(stream.toArray()).toEqual([1, 2, 3]);
+    });
+
+    it('returns empty array', () => {
+      const stream = new Stream();
+      expect(stream.isEmpty()).toBeTruthy();
+    });
+
+    it('returns array with one item', () => {
+      const stream = new Stream(1);
+      expect(stream.toArray()).toEqual([1]);
+    });
+
+    it('returns array with two items', () => {
+      const stream = new Stream(1, () => new Stream(2));
+      expect(stream.toArray()).toEqual([1, 2]);
+    });
+  });
+
+  describe('filter', () => {
+    it('returns empty stream', () => {
+      const stream = Stream.fromArray([1, 2, 3]);
+      const filtered = stream.filter(() => false);
+      expect(filtered.isEmpty()).toBeTruthy();
+    });
+
+    it('returns stream with one item', () => {
+      const stream = Stream.fromArray([1, 2, 3]);
+      const filtered = stream.filter(item => item === 2);
+      expect(filtered.toArray()).toEqual([2]);
+    });
+
+    it('returns stream multiple items', () => {
+      const stream = Stream.fromArray([1, 2, 3]);
+      const filtered = stream.filter(item => item > 1);
+      expect(filtered.toArray()).toEqual([2, 3]);
+    });
+  });
+
+  describe('map', () => {
+    it('returns empty stream', () => {
+      const stream = new Stream();
+      const mapped = stream.map(item => item + 1);
+      expect(mapped.isEmpty()).toBeTruthy();
+    });
+
+    it('returns stream with one item', () => {
+      const stream = new Stream(1);
+      const mapped = stream.map(item => item + 1);
+      expect(mapped.toArray()).toEqual([2]);
+    });
+
+    it('maps items', () => {
+      const stream = Stream.fromArray([1, 2, 3]);
+      const mapped = stream.map(item => item + 1);
+      expect(mapped.toArray()).toEqual([2, 3, 4]);
+    });
+  });
+
+  describe('concat', () => {
+    it('returns empty stream', () => {
+      const stream = new Stream();
+      const concat = stream.concat(new Stream());
+      expect(concat.isEmpty()).toBeTruthy();
+    });
+
+    it('concats two streams', () => {
+      const stream1 = Stream.fromArray([1, 2, 3]);
+      const stream2 = Stream.fromArray([4, 5, 6]);
+      const concat = stream1.concat(stream2);
+      expect(concat.toArray()).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+  });
+
+  describe('length', () => {
+    it('returns 0 for empty stream', () => {
+      const stream = new Stream();
+      expect(stream.length()).toEqual(0);
+    });
+
+    it('returns 1 for stream with one item', () => {
+      const stream = new Stream(1);
+      expect(stream.length()).toEqual(1);
+    });
+
+    it('returns 3 for stream with three items', () => {
+      const stream = Stream.fromArray([1, 2, 3]);
+      expect(stream.length()).toEqual(3);
+    });
+  });
+
+  describe('reduce', () => {
+    it('returns empty stream', () => {
+      const stream = new Stream();
+      expect(stream.reduce((acc, item) => acc + item, 0)).toEqual(0);
+    });
+
+    it('reduces stream', () => {
+      const stream = Stream.fromArray([1, 2, 3]);
+      expect(stream.reduce((acc, item) => acc + item, 0)).toEqual(6);
+    });
+  });
+
+  describe('fromRange', () => {
+    it('creates a stream from range', () => {
+      const stream = Stream.fromRange(1, 3);
+      expect(stream.toArray()).toEqual([1, 2, 3]);
+    });
+
+    it('creates a stream from range with step', () => {
+      const stream = Stream.fromRange(1, 3, 2);
+      expect(stream.toArray()).toEqual([1, 3]);
+    });
+  });
+
+  describe('chaining', () => {
+    it('can chain filter and first', () => {
+      const stream = Stream.fromArray([1, 2, 3]);
+      const filtered = stream.filter(item => item > 1).first();
+      expect(filtered).toEqual(2);
+    });
+
+    it('can chain filter and toArray', () => {
+      const stream = Stream.fromArray([1, 2, 3]);
+      const filtered = stream.filter(item => item > 1).toArray();
+      expect(filtered).toEqual([2, 3]);
+    });
+
+    it('can chain map and first', () => {
+      const stream = Stream.fromArray([1, 2, 3]);
+      const mapped = stream.map(item => item + 1).first();
+      expect(mapped).toEqual(2);
+    });
+
+    it('chains filter and map in correct order', () => {
+      const stream = Stream.fromArray([1, 2, 3]);
+      const filtered = stream.filter(item => item > 1).map(item => item + 1);
+      expect(filtered.toArray()).toEqual([3, 4]);
+    });
+
+    it('calls filter only until first item is found when using first', () => {
+      const filterMock = jest.fn(() => true);
+      const stream = Stream.fromArray([1, 2, 3]);
+      const filtered = stream.filter(filterMock).first();
+      expect(filterMock).toHaveBeenCalledTimes(1);
+    });
+
+    it('calls map only until first item is found when using first', () => {
+      const mapMock = jest.fn(() => 1);
+      const stream = Stream.fromArray([1, 2, 3]);
+      const mapped = stream.map(mapMock).first();
+      expect(mapMock).toHaveBeenCalledTimes(1);
+    });
+
+    it('calls multiple filters until first item is found when using first', () => {
+      const evenFilterMock = jest.fn(item => item % 2 === 0);
+      const divisibleBy3FilterMock = jest.fn(item => item % 3 === 0);
+      const stream = Stream.fromArray([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      const filtered = stream
+        .filter(evenFilterMock)
+        .filter(divisibleBy3FilterMock)
+        .first();
+      expect(evenFilterMock).toHaveBeenCalledTimes(6);
+      expect(divisibleBy3FilterMock).toHaveBeenCalledTimes(3);
+    });
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,152 @@
+class Stream {
+  private head: any;
+  private generator: () => Stream;
+
+  constructor(head: any = null, generator: () => Stream = () => new Stream()) {
+    this.head = head;
+    this.generator = generator;
+  }
+
+  /**
+   * @returns First item of the stream.
+   * @throws {Error} If stream is empty.
+   */
+  first(): any {
+    if (this.head == null) {
+      throw new Error('Stream is empty');
+    }
+    return this.head;
+  }
+
+  /**
+   * Checks if stream is empty.
+   * 
+   * @returns True if stream is empty.
+   */
+  isEmpty(): boolean {
+    return this.head == null;
+  }
+  
+  /**
+   * Collects stream items into an array.
+   * 
+   * @returns Array of stream items.
+   */ 
+  toArray(): any[] {
+    let result: any[] = [];
+    let stream: Stream = this;
+    while (!stream.isEmpty()) {
+      result.push(stream.head);
+      stream = stream.generator();
+    }
+    return result;
+  }
+
+  /**
+   * Filters stream items by predicate.
+   * 
+   * @param predicate Predicate function.
+   * @returns Filtered stream.
+   */
+  filter(predicate: (item: any) => boolean): Stream {
+    if (this.isEmpty()) {
+      return this;
+    }
+
+    if (predicate(this.head)) {
+      return new Stream(this.head, () => this.generator().filter(predicate));
+    } else {
+      return this.generator().filter(predicate);
+    }
+  }
+
+  /**
+   * Maps stream items by mapper.
+   * 
+   * @param mapper Mapper function.
+   * @returns Mapped stream.
+   */
+  map(mapper: (item: any) => any): Stream {
+    if (this.isEmpty()) {
+      return this;
+    }
+
+    return new Stream(mapper(this.head), () => this.generator().map(mapper));
+  }
+
+  /**
+   * Concatenates two streams.
+   * 
+   * @param stream Stream to concatenate.
+   * @returns Concatenated stream.
+   */
+  concat(stream: Stream): Stream {
+    if (this.isEmpty()) {
+      return stream;
+    }
+
+    return new Stream(this.head, () => this.generator().concat(stream));
+  }
+
+  /**
+   * Returns stream length.
+   * 
+   * @returns Stream length.
+   */
+  length(): number {
+    let length: number = 0;
+    let stream: Stream = this;
+    while (!stream.isEmpty()) {
+      length++;
+      stream = stream.generator();
+    }
+    return length;
+  }
+
+  /**
+   * Reduces stream items by reducer.
+   * 
+   * @param reducer Reducer function.
+   * @param initialValue Initial value.
+   * @returns Reduced value.
+   */
+  reduce(reducer: (acc: any, item: any) => any, initialValue: any): any {
+    let acc: any = initialValue;
+    let stream: Stream = this;
+    while (!stream.isEmpty()) {
+      acc = reducer(acc, stream.head);
+      stream = stream.generator();
+    }
+    return acc;
+  }
+
+  /**
+   * Creates a stream from a range.
+   * 
+   * @param start Start value.
+   * @param end End value.
+   * @param step Step value.
+   * @returns Stream.
+   */
+  static fromRange(start: number, end: number, step: number = 1): Stream {
+    if (start === end) {
+      return new Stream(start);
+    }
+    return new Stream(start, () => Stream.fromRange(start + step, end, step));
+  }
+
+  /**
+   * Creates a stream from an array.
+   * 
+   * @param array Array.
+   * @returns Stream.
+   */
+  static fromArray(array: any[]): Stream {
+    if (array.length === 0) {
+      return new Stream();
+    }
+    return new Stream(array[0], () => Stream.fromArray(array.slice(1)));
+  }
+}
+
+export default Stream;
